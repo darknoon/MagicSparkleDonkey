5472291518e6e1d81dd1b0f3a649cbc5f996e5db
Andrew Pouliot <andrew@darknoon.com>
2022-05-01 17:56:19 -0400

    Apply transforms from parent-child through hierarchy

--- MSD/Sources/MSD/Systems/RenderSystem.swift
+++ MSD/Sources/MSD/Systems/RenderSystem.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Andrew Pouliot on 4/22/21.
 //
+import simd
 
 public struct MeshComponent : Component {
     public let resource: Resource.ID?
@@ -30,12 +31,32 @@ public class RenderSystem : System {
     
     public func update(stepInfo: StepInfo, scene: Scene) {
         displayList.removeAll()
+        
+        var currentTransform: Transform = .identity
 
-        scene.store.forEach{(id, transform: inout TransformComponent, mesh: inout MeshComponent) in
-            let t = transform.transform
-            if let m = mesh.resource {
-                displayList.append(Display(transform: t, resource: m))
+        func traverse(childrenOf entity: Entity.ID) {
+            // push transform
+            let previousTransform = currentTransform
+            if let transform = (scene.store[entity] as TransformComponent?)?.transform {
+                currentTransform = simd_mul(currentTransform, transform)
             }
+
+            if let mesh = scene.store[entity] as MeshComponent?, let meshResource = mesh.resource {
+                let d = Display(transform: currentTransform, resource: meshResource)
+                displayList.append(d)
+            }
+            
+            // get this entity's children (if any)
+            if let children = scene.store[entity] as EntityChildCollection? {
+                for child in children {
+                    // Add children to render list
+                    traverse(childrenOf: child)
+                }
+            }
+            // pop transform
+            currentTransform = previousTransform
         }
+
+        traverse(childrenOf: scene.root)
     }
 }
